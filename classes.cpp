830a25f4f2b806c9b7ee8af69c316971e5136a36
Ensar Gök <ensarxgok@gmail.com>
2023-08-14 19:00:29 +0300

    More use of classes
    Using pointers, heap and operators with classes.

--- classes.cpp
+++ classes.cpp
@@ -9,6 +9,11 @@ public:
         this->age = age;
     }
 
+    ~Person()
+    {
+        std::cout << "Destructor called for " << name << std::endl;
+    }
+
     void print()
     {
         std::cout << "Name: " << name << ", Age: " << age << std::endl;
@@ -17,6 +22,16 @@ public:
     void changeName(const char* name)
     {
         this->name = name;
+    }
+
+    operator int()
+    {
+        return age;
+    }
+
+    int operator + (const Person& other)
+    {
+        return age + other.age;
     }
 
 private:
@@ -31,5 +46,41 @@ int main()
     p.print();
     p.changeName("Mehmet");
     p.print();
+
+    {
+        Person p2("Ali", 30);
+        p2.print();
+    }
+    // destructor is called for p2
+    //  p2.print(); // Error: p2 is not defined in this scope
+
+    // Create a new scope and create a new Person object in heap
+    // and access it out of the scope
+    Person* p3 = nullptr;
+    {
+        p3 = new Person("Veli", 40);
+        p3->print();
+    }
+    // destructor is not called for p3
+
+    p3->print();
+
+    std::cout << "Age of p3: " << (int)*p3 << std::endl;
+
+    /*
+    Person person3 = *p3;
+    person3.print();
+    
+    std::cout << "Age of person3: " << (int)person3 << std::endl;
+    std::cout << "Age of p3: " << int(person3) << std::endl;
+    */
+
+    delete p3; // destructor is called for p3
+
+    Person p4("Ayse", 50);
+    std::cout << "Age of p4: " << (int)p4 << std::endl;
+
+    std::cout << "Sum of ages: " << p + p4 << std::endl;
+
     return 0;
 }
